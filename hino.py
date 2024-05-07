19dac6bc226e0c78f10414dc54a8e1ce4536af30
shoco <119436427+916shoco@users.noreply.github.com>
2024-05-07 15:45:37 -0300

    Update hino.py

--- hino.py
+++ hino.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import asyncio
 
 intents = discord.Intents.default()
 intents.members = True
@@ -20,7 +21,13 @@ async def on_member_join(member):
                               description="<a:B_BlueLacoGif:1198123120628867183> Oba! Membro novo na área! <a:B_BlueLacoGif:1198123120628867183>",
                               color=0xFFFFFF)
         embed.set_image(url="https://cdn.discordapp.com/attachments/1068578811345637486/1206738008485793863/718_Sem_Titulo4_20240212200524.png?ex=65dd1959&is=65caa459&hm=ad63e9716bf24516713e96e2590f76f7d5630838f2d5abcc3df19fdc41f4208d&")
-        await channel.send(message_content, embed=embed)
+        message = await channel.send(message_content, embed=embed)
+
+        # Espera 1 minuto
+        await asyncio.sleep(60)
+
+        # Apaga a mensagem após 1 minuto
+        await message.delete()
     else:
         print("Chat nao encontrado")
 
