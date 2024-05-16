95cbd727130b9f5e9f85eecb3bf83569c3d24ec2
shoco <119436427+916shoco@users.noreply.github.com>
2024-05-15 21:13:57 -0300

    Update invitechecker.py

--- cogs/invitechecker.py
+++ cogs/invitechecker.py
@@ -3,7 +3,7 @@ import aiosqlite
 from discord.ext import commands, tasks
 from discord import Status, Activity, ActivityType
 
-invite_code = "https://discord.gg/rtmFSVNU9z"
+invite_codes = ["discord.gg/rtmFSVNU9z", "gg/shadowsrealm"]
 role_id = 1198347965828444257
 
 DATABASE_PATH = f"{os.path.realpath(os.path.dirname(__file__))}/../database/members.db"
@@ -37,15 +37,17 @@ class StatusChecker(commands.Cog):
                 member_ids = [row[0] for row in await cursor.fetchall()]
                 for guild in self.bot.guilds:  
                     for member in guild.members:
+                        if member.status == Status.offline:
+                            continue  # Skip this member if they are offline
                         if member.id in member_ids:
-                            if not member.activity or member.activity.type != ActivityType.custom or invite_code not in member.activity.name:
+                            if not member.activity or member.activity.type != ActivityType.custom or not any(invite_code in member.activity.name.lower().split() for invite_code in invite_codes):
                                 await cursor.execute('DELETE FROM members WHERE member_id = ?', (member.id,))
                                 if await has_role(member, role_id):
                                     await member.remove_roles(member.guild.get_role(role_id))
-                        elif member.status in online_statuses and member.activity and member.activity.type == ActivityType.custom and invite_code in member.activity.name:
+                        elif member.activity and member.activity.type == ActivityType.custom and any(invite_code in member.activity.name.lower().split() for invite_code in invite_codes):
                             await cursor.execute('INSERT INTO members (member_id) VALUES (?)', (member.id,))
                             if not await has_role(member, role_id):
-                                await member.add_roles(member.guild.get_role(role_id)))
+                                await member.add_roles(member.guild.get_role(role_id))
                 await conn.commit()
         except Exception as e:
             print(f"Ocorreu um erro: {e}")
